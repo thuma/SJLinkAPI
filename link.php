9038aff01475799f5739dc6802ab308a2c0d0bf0
Martin Thuresson <martin.thure@gmail.com>
2014-01-19 17:32:15 +0100

    Added information

---
+++ link.php
@@ -0,0 +1,29 @@
+from = B
+to = c
+
+NNNNNN:CCC
+
+C = country
+N = Stationid
+
+depDate = f (g=DEPARTURE_DATE_TIME)				arrDate = f (g=ARRIVAL_DATE_TIME)
+depTime = F										arrTime = F
+
+YYYYMMDD
+HHMM
+
+(G=true, G = false vid enkelresa)
+depDateReturn = h (i=DEPARTURE_DATE_TIME)		arrDateReturn = h (i=ARRIVAL_DATE_TIME) 
+depTimeReturn = H								arrTimeReturn = H
+
+travelers = N (mA,MA,nA,NA,oA,OA,pA,PA,qA)
+
+VU Vuxen  (25-)
+UN Ungdom (20-25)
+U1 Ungdom (16-19)
+ST Student
+B6 Barn (7-15)
+BA Barn (0-6)
+
+
+
