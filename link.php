a2db66475cf376af2a287ada1c9f4b2756208def
Martin Thuresson <martin.thure@gmail.com>
2014-01-20 17:55:56 +0100

    Added first code

--- link.php
+++ link.php
@@ -1,10 +1,11 @@
+<?php
+/*
 from = B
 to = c
 
-NNNNNN:CCC
-
-C = country
-N = Stationid
+NNNNN:CCC
+	C = country
+	N = Stationid
 
 depDate = f (g=DEPARTURE_DATE_TIME)				arrDate = f (g=ARRIVAL_DATE_TIME)
 depTime = F										arrTime = F
@@ -12,18 +13,101 @@ depTime = F										arrTime = F
 YYYYMMDD
 HHMM
 
-(G=true, G = false vid enkelresa)
+	(G=true, G=false vid enkelresa)
 depDateReturn = h (i=DEPARTURE_DATE_TIME)		arrDateReturn = h (i=ARRIVAL_DATE_TIME) 
 depTimeReturn = H								arrTimeReturn = H
 
 travelers = N (mA,MA,nA,NA,oA,OA,pA,PA,qA)
 
-VU Vuxen  (25-)
-UN Ungdom (20-25)
-U1 Ungdom (16-19)
-ST Student
-B6 Barn (7-15)
-BA Barn (0-6)
+VU Vuxen   (25---)
+UN Ungdom  (20-25)
+U1 Ungdom  (16-19)
+ST Student (75%ST)
+B6 Barn    (07-15)
+BA Barn    (00-06)
+
+link.php?from=00001:074&to=00002:074&depDate2013-03-03&depTime=12:00&travelers=VU,UN,U1
+
+link.php?from=00001:074&to=00002:074&depDate2013-03-03&depTime=12:00&travelers=VU,UN,U1
 
 
+*/
 
+// Convert from national number to split id and national number
+$from = substr($_GET['from'], -5).'%3A0'.substr($_GET['from'], 0,2); 
+$to = substr($_GET['to'], -5).'%3A0'.substr($_GET['to'], 0,2);
+
+// Generate url
+$url = 	'http://www.sj.se/microsite/microsite/submit.form?'. 	// Base url
+		'&B='.$from.											// From stop NNNNN:CCC, C = country, N = Stationid
+		'&c='.$to. 												// To stop NNNNN:CCC, C = country, N = Stationid
+		'header.key=K253891809275136476'.						// Some key identifying app (May change)
+		'&l=sv'.												// Lanuage
+		'&3A=false'; 											// Dont know whats this is but it seams to be nessesary.
+		
+// Gheck if departure or arrival:
+if($isset($_GET['depDate']) AND $isset($_GET['depTime']))
+	{
+	$time = substr($_GET['depTime']0,2).'00';
+	$date = preg_replace('/-/','',$_GET['depDate']);
+	$url = .'&g=DEPARTURE_DATE_TIME'.
+			'&f='.$date;
+			'&F='.$time;
+	}
+	
+elseif($isset($_GET['arrDate']) AND $isset($_GET['arrTime']))
+	{
+	$time = substr($_GET['arrTime']0,2).'00';
+	$date = preg_replace('/-/','',$_GET['arrDate']);
+	$url = .'&g=ARRIVAL_DATE_TIME'.
+			'&f='.$date;
+			'&F='.$time;
+	}
+	
+else
+	{
+	die('Error: no depDate/arrDate or depTime/arrTime set.');
+	}
+
+// Check if returntrip is set:
+if($isset($_GET['depDateReturn']) AND $isset($_GET['depTimeReturn']))
+	{
+	$time = substr($_GET['depTimeReturn']0,2).'00';
+	$date = preg_replace('/-/','',$_GET['depDateReturn']);
+	$url = .'&i=DEPARTURE_DATE_TIME'.
+			'&G=true'.
+			'&h='.$date;
+			'&H='.$time;
+	}
+	
+elseif($isset($_GET['arrDateReturn']) AND $isset($_GET['arrTimeReturn']))
+	{
+	$time = substr($_GET['arrTimeReturn']0,2).'00';
+	$date = preg_replace('/-/','',$_GET['arrDate']);
+	$url = .'&i=ARRIVAL_DATE_TIMEReturn'.
+			'&G=true'.
+			'&h='.$date;
+			'&H='.$time;
+	}
+	
+else
+	{
+	$url = .'&G=false';
+	}
+
+//Handle travelers:	
+if(isset($_GET['travelers']) == false){
+die('Error: value of travelers is not set.');
+}
+$travelers = preg_split('/,/',$_GET['travelers']);
+$url = . '&N='.count($travelers);
+
+$codename = array('mA','MA','nA','NA','oA','OA','pA','PA','qA');
+
+foreach($travelers as $key => $traveler){
+$url = .'&'.$codename.'='.$traveler;
+}
+
+die($url);	
+header('Location: '.$url);
+?>
